cea1cadae40bf861985779dec026b0a7ad8f37eb
Lars Boß <lars.boss@prodyna.com>
2022-10-18 15:45:29 +0200

    integrated library- and library-assets-path into application

--- projects/test-sample-module/src/app/app-routing.module.ts
+++ projects/test-sample-module/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AtropossSampleModuleComponent } from 'projects/atroposs-sample-module/src/public-api';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AtropossSampleModuleComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
