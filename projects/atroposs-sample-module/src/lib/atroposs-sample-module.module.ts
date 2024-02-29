91c9328bef20d9a2600eadd0b19f0cd2aafb85db
Lars Boß <lars.boss@prodyna.com>
2024-02-29 15:15:25 +0100

    Added TestThemeComponent

--- projects/atroposs-sample-module/src/lib/atroposs-sample-module.module.ts
+++ projects/atroposs-sample-module/src/lib/atroposs-sample-module.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core'
 import { AtropossSampleModuleComponent } from './atroposs-sample-module.component'
 import { MaterialModule } from './modules/material.module'
-import { LibRoutingModule } from './modules/lib-routing.module'
+import { LibRoutingModule } from './modules/lib-routing.module';
+import { TestThemeMaterialComponent } from './components/test-theme-material/test-theme-material.component'
 
 @NgModule({
-  declarations: [AtropossSampleModuleComponent],
+  declarations: [AtropossSampleModuleComponent, TestThemeMaterialComponent],
   imports: [MaterialModule, LibRoutingModule],
   exports: [AtropossSampleModuleComponent],
 })
