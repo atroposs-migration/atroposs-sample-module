a87dd2a93dd1eb3f32b7b18e94ee763fa08acabe
Lars Boß <lars.boss@prodyna.com>
2024-04-11 11:41:37 +0200

    Modified module header

--- projects/atroposs-sample-module/src/lib/modules/lib-routing.module.ts
+++ projects/atroposs-sample-module/src/lib/modules/lib-routing.module.ts
@@ -2,16 +2,34 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { AtropossSampleModuleComponent } from '../atroposs-sample-module.component'
 import { TestThemeMaterialComponent } from '../components/test-theme-material/test-theme-material.component'
+// @ts-expect-error
+import packageJson from '../../../package.json'
 
 const lib_routes: Routes = [
   {
     path: 'spm-module', // root-routing path
-    component: AtropossSampleModuleComponent, // root-routing component
+    data: {
+      name: 'Atroposs-Sample-Module',
+      version: packageJson.version,
+      icon: {
+        src: 'spm-assets/example/atroposs-sample-icon.png',
+        alt: 'Atroposs Sample Icon',
+      },
+      imagePaths: [
+        {
+          src: 'spm-assets/example/atroposs-sample-logo.png',
+          alt: 'Atroposs Sample Logo',
+        },
+      ],
+    }, // root-routing data
     children: [
       {
-        path: '',
+        path: '', // child-routing path
+        component: AtropossSampleModuleComponent, // root-routing component
+      },
+      {
+        path: 'test-theme-material', // child-routing path
         component: TestThemeMaterialComponent,
-        outlet: 'lib-outlet',
       },
     ],
   },
